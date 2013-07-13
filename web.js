c9e05286e844c812f13a756bc68cdd4f04cc4577
heroku <git@heroku.com:evening-island-9723.git>
2013-07-13 19:52:10 +0200

    web.js reads from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var fs = require('fs');
 var app = express();
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
+
+var html_file = "./index.html"
+var str_to_send = decoder.write(fs.readFileSync(html_file));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str_to_send);
 });
 
 var port = process.env.PORT || 5000;
