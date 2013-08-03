fac943d49a25e7f3a5b3cfbc11aedbb445a5bfcf
marek007 <marek007@gmail.com>
2013-08-03 18:36:01 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(str_to_send);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
